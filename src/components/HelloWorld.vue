1fa9054e8870802133455918623e63a506255e2a
Axel Ridray <axel.ridray@sfr.fr>
2021-07-06 16:31:38 +0200

    premiers pas

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,32 +1,10 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+    <p v-if="abonnes == 0">aucun abonnés pour le moment ^^'</p>
+    <p v-else-if="abonnes == 1">une personne s'est abonnée</p>
+    <p v-else>{{abonnes}} personnes se sont abonnées</p>
+    <button class="button" v-on:click="subscribe()">S'abonner !</button>
   </div>
 </template>
 
@@ -35,6 +13,16 @@ export default {
   name: 'HelloWorld',
   props: {
     msg: String
+  },
+  data:function () {
+    return {
+      abonnes: 0,
+    }
+  },
+  methods: {
+    subscribe: function () {
+      this.abonnes++;
+    }
   }
 }
 </script>
@@ -54,5 +42,14 @@ li {
 }
 a {
   color: #42b983;
+}
+p {
+  font-size: 36px;
+}
+.button {
+  background-color: red;
+  border-radius: 6px;
+  color: aliceblue;
+  bottom: 1px;
 }
 </style>
